1d94c47d1a9b7802306446853593147e194c5cf2
Sai Rajath <sairajath7@gmail.com>
2025-08-08 14:47:42 +0530

    Optimize imports

--- lab/16-annotations/src/main/java/config/RewardsConfig.java
+++ lab/16-annotations/src/main/java/config/RewardsConfig.java
@@ -1,19 +1,7 @@
 package config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import rewards.RewardNetwork;
-import rewards.internal.RewardNetworkImpl;
-import rewards.internal.account.AccountRepository;
-import rewards.internal.account.JdbcAccountRepository;
-import rewards.internal.restaurant.JdbcRestaurantRepository;
-import rewards.internal.restaurant.RestaurantRepository;
-import rewards.internal.reward.JdbcRewardRepository;
-import rewards.internal.reward.RewardRepository;
-
-import javax.sql.DataSource;
 
 /**
  * TODO-07: Perform component-scanning and run the test again
